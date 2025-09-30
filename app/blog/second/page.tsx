7a68573ff1d212dc06bd43595eba0945473c1590
jenny <zhen@solution22.com.au>
2025-09-30 13:39:40 +1000

    Delete app directory

--- app/blog/second/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function SecondBlog(){
-    return <h1>Second blog post</h1>;
-}
--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-function BarChart() {
-    return <h1> Bar Chart </h1>;
-}
-
-export default function Dashboard() {
-    return <h1> Dahsboard </h1>;
-}
